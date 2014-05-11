116b41d8afa87777362e5bb928020c36c200e406
Florian <florian.schwanz@interoberlin.de>
2014-05-11 03:32:19 +0200

    Added renderer for path moveto and lineto

--- src/de/interoberlin/sauvignon/model/util/Vector2.java
+++ src/de/interoberlin/sauvignon/model/util/Vector2.java
@@ -5,10 +5,22 @@ public class Vector2
 	private float	x;
 	private float	y;
 
+	public Vector2(Vector2 v)
+	{
+		this.setX(v.getX());
+		this.setY(v.getY());
+	}
+
 	public Vector2(float x, float y)
 	{
 		this.setX(x);
 		this.setY(y);
+	}
+
+	public Vector2()
+	{
+		this.setX(0.0f);
+		this.setY(0.0f);
 	}
 
 	public float getX()

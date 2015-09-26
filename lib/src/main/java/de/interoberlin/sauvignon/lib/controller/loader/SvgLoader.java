70fa6009cb060ac6794d72ad1a2a8c69b72bd45d
Florian <florian.schwanz@interoberlin.de>
2015-09-27 00:22:53 +0200

    Added svg file loader

--- lib/src/main/java/de/interoberlin/sauvignon/lib/controller/loader/SvgLoader.java
+++ lib/src/main/java/de/interoberlin/sauvignon/lib/controller/loader/SvgLoader.java
@@ -9,23 +9,36 @@ import java.io.IOException;
 import de.interoberlin.sauvignon.lib.controller.parser.SvgHandler;
 import de.interoberlin.sauvignon.lib.model.svg.SVG;
 
-public class SvgLoader
-{
-	public static SVG getSVGFromAsset(Context c, String svgPath)
-	{
-		try
-		{
-			return SvgHandler.getSVGFromAsset(c.getAssets(), svgPath);
-		} catch (IOException e)
-		{
-			System.out.println(e.toString());
-			e.printStackTrace();
-		} catch (XmlPullParserException e)
-		{
-			System.out.println(e.toString());
-			e.printStackTrace();
-		}
+public class SvgLoader {
+    // --------------------
+    // Methods
+    // --------------------
 
-		return null;
-	}
+    public static SVG getSVGFromAsset(Context c, String svgPath) {
+        try {
+            return SvgHandler.getSVGFromAsset(c.getAssets(), svgPath);
+        } catch (IOException e) {
+            System.out.println(e.toString());
+            e.printStackTrace();
+        } catch (XmlPullParserException e) {
+            System.out.println(e.toString());
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static SVG getSVGFromFile(String svgPath) {
+        try {
+            return SvgHandler.getSVGFromFile(svgPath);
+        } catch (IOException e) {
+            System.out.println(e.toString());
+            e.printStackTrace();
+        } catch (XmlPullParserException e) {
+            System.out.println(e.toString());
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
--- lib/src/main/java/de/interoberlin/sauvignon/lib/controller/parser/SvgHandler.java
+++ lib/src/main/java/de/interoberlin/sauvignon/lib/controller/parser/SvgHandler.java
@@ -6,28 +6,33 @@ import android.content.res.Resources.NotFoundException;
 
 import org.xmlpull.v1.XmlPullParserException;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 import de.interoberlin.sauvignon.lib.model.svg.SVG;
 
-public class SvgHandler
-{
-	public static SVG getSVGFromInputStream(InputStream svgData) throws XmlPullParserException, IOException
-	{
-		return SvgParser.getInstance().parse(svgData);
-	}
+public class SvgHandler {
+    public static SVG getSVGFromInputStream(InputStream svgData) throws XmlPullParserException, IOException {
+        return SvgParser.getInstance().parse(svgData);
+    }
 
-	public static SVG getSVGFromResource(Resources resources, int resId) throws NotFoundException, XmlPullParserException, IOException
-	{
-		return SvgParser.getInstance().parse(resources.openRawResource(resId));
-	}
+    public static SVG getSVGFromResource(Resources resources, int resId) throws NotFoundException, XmlPullParserException, IOException {
+        return SvgParser.getInstance().parse(resources.openRawResource(resId));
+    }
 
-	public static SVG getSVGFromAsset(AssetManager assetMngr, String svgPath) throws IOException, XmlPullParserException
-	{
-		InputStream inputStream = assetMngr.open(svgPath);
-		SVG svg = getSVGFromInputStream(inputStream);
-		inputStream.close();
-		return svg;
-	}
+    public static SVG getSVGFromAsset(AssetManager assetMngr, String svgPath) throws IOException, XmlPullParserException {
+        InputStream inputStream = assetMngr.open(svgPath);
+        SVG svg = getSVGFromInputStream(inputStream);
+        inputStream.close();
+        return svg;
+    }
+
+    public static SVG getSVGFromFile(String svgPath) throws IOException, XmlPullParserException {
+        InputStream inputStream = new FileInputStream(new File(svgPath));
+        SVG svg = getSVGFromInputStream(inputStream);
+        inputStream.close();
+        return svg;
+    }
 }
